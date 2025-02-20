c471abaa09c4925b4193850690f43bdbd8d47579
arshita <arshitapatel077@gmail.com>
2025-02-20 09:54:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+function detail(){
+    //console.log("Test case 123");
+   // let btnRef = document.getElementById('abc')
+    //console.log(btnRef)
+    let btnRef = document.getElementById('unit 1');
+    console.log(btnRef)
+}
